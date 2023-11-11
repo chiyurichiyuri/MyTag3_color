50d5e6d4ce51196ed9bf01e89817fb3f2d4a620a
chiyurichiyuri <150512764+chiyurichiyuri@users.noreply.github.com>
2023-11-11 17:41:21 +0900

    Add files via upload

---
+++ select.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+
+def name():
+    name = ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7"]
+    return random.choice(name)
+meishi = name()
+
+print(meishi, "を実行してください。")
+
+
+# In[ ]:
+
+
+
+
---
+++ tag2.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+
+title = "ChiyuriRed TiyuriRed"
+meishi = "@長山 ちゆり"
+
+print(title, meishi)
+
+def tag1():
+    tag1 = ["chiyuri.tiyuri", "tiyuri.chiyuri"]
+    return random.choice(tag1)
+title1 = tag1()
+
+def tag2():
+    tag2 = ["chiyuri_tiyuri", "tiyuri_chiyuri"]
+    return random.choice(tag2)
+title2 = tag2()
+
+def tag3():
+    tag3 = ["chiyuritiyuri", "tiyurichiyuri"]
+    return random.choice(tag3)
+title3 = tag3()
+
+def tag4():
+    tag4 = ["chiyuri.chiyuri", "tiyuri.tiyuri"]
+    return random.choice(tag4)
+title4 = tag4()
+
+def tag5():
+    tag5 = ["chiyuri_chiyuri", "tiyuri_tiyuri"]
+    return random.choice(tag5)
+title5 = tag5()
+
+def tag6():
+    tag6= ["chiyurichiyuri", "tiyuritiyuri"]
+    return random.choice(tag6)
+title6= tag6()
+
+print(title1, title2, title3, title4, title5, title6)
+
+def comment():
+    comment = ["I'm doing well today.", "It's going well today.", "I am having a good time today."]
+    return random.choice(comment)
+subtitle = comment()
+
+def name():
+    name = ["Nagayama Chiyuri", "Chiyuri Nagayama"]
+    return random.choice(name)
+meishi = name()
+
+print(subtitle, "by", meishi)
+
+
+# In[ ]:
+
+
+
+
